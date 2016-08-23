8b49589136830c3aecc9d5d66a7a1918c132e8bf
D4md1n <d4md1n@hotmail.com>
2016-08-24 01:23:07 +0300

    added functionality to attribute with maximum gain

--- src/main/scala-2.11/SparkTest.scala
+++ src/main/scala-2.11/SparkTest.scala
@@ -12,6 +12,7 @@ object SparkTest {
   val measurementAttributes: Int = 14
   val decisionAttribute: Int = 0
   var numberOfMeasurements: Long = 0
+  var decisionAttributeEntropy: Double = 0.0
 
 
   val conf = new SparkConf()
@@ -32,26 +33,42 @@ object SparkTest {
 //    probabilityRDD.foreach(v => println(v))
     //getProbabilitiesByAttribute(probabilityRDD).foreach(v => println(v))
 
-    var decisionAttrEntropy = oneAttributeEntropy(classifiedMeasurements,decisionAttribute)
-    println(decisionAttrEntropy)
+    decisionAttributeEntropy = oneAttributeEntropy(classifiedMeasurements,decisionAttribute)
     val attr1 = 8
-    val attr1EntropyOnDecisionAttr =
-      classifiedMeasurements
-        .map(v => ((v.values(attr1), v.values(decisionAttribute)), 1))
-        .reduceByKey((v1,v2) => v1 + v2)
-        .map(v => (v._1 ,v._2/numberOfMeasurements.toDouble))
-        .groupBy(v => v._1._1)
-        .map(v => v._2
-          .map(v => (v._2, entropy(v._2)))
-          .reduce((v1,v2) => (v1._1 + v2._1, v1._2 + v2._2)))
-        .map(v => v._1 * v._2).sum()
+    val attr1EntropyOnDecisionAttr = getEntropyOverDecisionAttribute(classifiedMeasurements, attr1)
 
-        //.map(v => entropy(v._2)).sum()
-
-    val gain = decisionAttrEntropy - attr1EntropyOnDecisionAttr
-    println(s"the decision Attribute entropy is $decisionAttrEntropy " +
+    val gain = decisionAttributeEntropy - attr1EntropyOnDecisionAttr
+    println(s"the decision Attribute entropy is $decisionAttributeEntropy " +
       s"the attr1 entropy over decision attribute is $attr1EntropyOnDecisionAttr" +
       s" and the gain is $gain")
+
+    val maxGain = getAttributeWithMaximumGain(classifiedMeasurements)
+
+    println(s"the attribute with the maximum gain is $maxGain")
+  }
+
+  def getAttributeWithMaximumGain(classifiedMeasurements: RDD[ClassifiedMeasurement]): (Int, Double) = {
+    var maximumGain = (0,0.0)
+    for (i<-1 until measurementAttributes) {
+      val entropyOverDecisionAttribute = getEntropyOverDecisionAttribute(classifiedMeasurements, i)
+      val gain = decisionAttributeEntropy - entropyOverDecisionAttribute
+      if (gain > maximumGain._2) {
+        maximumGain = (i, gain)
+      }
+    }
+    maximumGain
+  }
+
+  def getEntropyOverDecisionAttribute(classifiedMeasurements: RDD[ClassifiedMeasurement], attribute: Int): Double = {
+    classifiedMeasurements
+      .map(v => ((v.values(attribute), v.values(decisionAttribute)), 1))
+      .reduceByKey((v1, v2) => v1 + v2)
+      .map(v => (v._1, v._2 / numberOfMeasurements.toDouble))
+      .groupBy(v => v._1._1)
+      .map(v => v._2
+        .map(v => (v._2, entropy(v._2)))
+        .reduce((v1, v2) => (v1._1 + v2._1, v1._2 + v2._2)))
+      .map(v => v._1 * v._2).sum()
   }
 
   def getAttributeWithMaximumEntropy(classifiedMeasurements: RDD[ClassifiedMeasurement]) : (Int, Double) = {
